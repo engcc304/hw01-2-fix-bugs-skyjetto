b5338ff2f73e05f72fd6a30f6ea33b927d3297b3
skyjetto <skyjettoshop@gmail.com>
2023-06-16 14:18:52 +0700

    comment scanf

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -1,8 +1,8 @@
 //TODO: #6 Fix all bugs.
 #include <stdio.h>
 int main() { 
-    int s = 10 ;
-    scanf( "%d", &s ) ;
+    // int s = 10 ;
+    // scanf( "%d", &s ) ;
     printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
     printf( "This is my first time to coding with C language.\n" ) ;
     printf( "Today is my birthday.\n" ) ;
