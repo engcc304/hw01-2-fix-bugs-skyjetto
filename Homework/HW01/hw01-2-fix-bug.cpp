6a6991b1089a3dad4ae37e5fd8c049fcaf1b6997
skyjetto <skyjettoshop@gmail.com>
2023-06-17 09:32:50 +0700

    change txt

--- Homework/HW01/hw01-2-fix-bug.cpp
+++ Homework/HW01/hw01-2-fix-bug.cpp
@@ -1,14 +1,8 @@
 //TODO: #6 Fix all bugs.
 #include <stdio.h>
 int main() { 
-    //int i = 0 ;
-    //scanf("%d", &i) ;
-    // printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
-    // printf( "This is my first time to coding with C language.\n" ) ;
-    // printf( "Today is my birthday.\n" ) ;
-    // printf( "Say \"Happy Birthday\" to me." ) ;
-
-    printf( "This is RMUTL.\n" ) ;
-    printf( "555" ) ;
+    printf( "Hello, I'm Javis (RMUTL-Version).\n" ) ;
+    printf( "This is my first time to coding with C language.\n" ) ;
+    printf( "Today is my birthday.\n" ) ;
     return 0 ;
 }
